19d2c317b30d0de43d533866d5237ad9a1a17b9a
Willy Rodriguez-Jurado <willy@rjurado.com>
2023-06-07 13:49:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,43 @@
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkins"
+# define GCP region
+variable "gcp_region" {
+  type        = string
+  description = "GCP region"
+  default     = "europe-west1"
+}
+# define GCP project name
+variable "gcp_project" {
+  type        = string
+  description = "GCP project name"
+  default     = "testing01-387705"
+}
+variable "bucket-name" {
+  type        = string
+  description = "The name of the Google Storage Bucket to create"
+  default     = "terraform-gcp-devops-test-372212a"
+}
+variable "storage-class" {
+  type        = string
+  description = "The storage class of the Storage Bucket to create"
+  default     = "NEARLINE"
+}
+
+terraform {
+  required_version = ">= 0.12"
+  # backend "gcs" {
+  # }
+}
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+# Create a GCS Bucket
+resource "google_storage_bucket" "tf-bucket" {
+  project       = var.gcp_project
+  name          = var.bucket-name
+  location      = var.gcp_region
+  force_destroy = true
+  storage_class = var.storage-class
+  versioning {
+    enabled = true
+  }
 }
