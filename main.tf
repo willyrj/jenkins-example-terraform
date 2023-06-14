b5e61aea5adc7bb4af662fee3895bb992363f1d5
Willy Rodriguez-Jurado <willy@rjurado.com>
2023-06-14 13:03:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@
 variable "gcp_region" {
   type        = string
   description = "GCP region"
-  default     = "europe-west1"
+  default     = "europe-west3"
 }
 # define GCP project name
 variable "gcp_project" {
   type        = string
   description = "GCP project name"
-  default     = "testing01-387705"
+  default     = "g-prj-afts-cs-uni-pro"
 }
 variable "bucket-name" {
   type        = string
