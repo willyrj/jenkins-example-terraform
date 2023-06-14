f005188150a7108574c6563551c82a0e1c6ea955
Willy Rodriguez-Jurado <willy@rjurado.com>
2023-06-14 13:11:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ variable "gcp_project" {
 variable "bucket-name" {
   type        = string
   description = "The name of the Google Storage Bucket to create"
-  default     = "terraform-gcp-devops-test-372212ac"
+  default     = "terraform-gcp-devops-test-66647"
 }
 variable "storage-class" {
   type        = string
